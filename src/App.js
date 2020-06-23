1b04aa6b03e2e24cc969849ac96014e2113bcf36
Michael Lauritsen <i30288741@live.dk>
2020-06-23 13:38:47 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React + Firebase Hosting + GCP Cloud Build, then Applying the GCP
-          Cloud Build CI/CD Pipeline
+          React + Firebase Hosting + GCP Cloud Build then Applying the GCP Cloud
+          Build CI/CD Pipeline
         </p>
         <a
           className="App-link"
